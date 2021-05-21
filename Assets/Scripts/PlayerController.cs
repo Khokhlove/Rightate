4a266dd00fcb7b76d5de8199cb5f02575bfa09b2
Khokhlove <52717700+Khokhlove@users.noreply.github.com>
2021-05-21 15:40:22 +0700

    Refactoring PlayerController!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,34 +14,53 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyUp(KeyCode.A)) Left();
         if (Input.GetKeyUp(KeyCode.W)) Up();
-        if (Input.GetKeyUp(KeyCode.S)) Right();
-        if (Input.GetKeyUp(KeyCode.D)) Down();
+        if (Input.GetKeyUp(KeyCode.D)) Right();
+        if (Input.GetKeyUp(KeyCode.S)) Down();
     }
     void _Move(Vector3 pos, Vector3 rot)
     {
         directionChanged.Invoke(direction, shapeController.currentShape);
-        //shapeController.currentShape.transform.position
+        StartCoroutine(Rotation(rot, shapeController.currentShape.gameObject));
+        StartCoroutine(Movement(pos, shapeController.currentShape.gameObject));
     }
 
+    IEnumerator Rotation(Vector3 rot, GameObject shapeObject)
+    {
+        Vector3 step = (rot - shapeObject.transform.rotation.eulerAngles) / (1 / Time.deltaTime);
+        while (Vector3.Distance(shapeObject.transform.rotation.eulerAngles, rot) >= 0.2)
+        {
+            shapeObject.transform.rotation = Quaternion.Euler(shapeObject.transform.rotation.eulerAngles + step);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    IEnumerator Movement(Vector3 pos, GameObject shapeObject)
+    {
+        Vector3 step = (pos - shapeObject.transform.position) / (1 / Time.deltaTime);
+        while (Vector3.Distance(shapeObject.transform.position, pos) >= 0.1)
+        {
+            shapeObject.transform.position += step;
+            yield return new WaitForEndOfFrame();
+        }
+    }
     public void Left()
     {
         direction = Direction.Left;
-        _Move(Vector3.zero, Vector3.zero);
+        _Move(new Vector3(-1, 0, 0), new Vector3(0,0,90));
     }
     public void Up()
     {
         direction = Direction.Up;
-        _Move(Vector3.zero, Vector3.zero);
+        _Move(new Vector3(0, 0, 1), new Vector3(90, 0, 0));
     }
     public void Right()
     {
         direction = Direction.Right;
-        _Move(Vector3.zero, Vector3.zero);
+        _Move(new Vector3(1, 0, 0), new Vector3(0, 0, -90));
     }
     public void Down()
     {
         direction = Direction.Down;
-        _Move(Vector3.zero, Vector3.zero);
+        _Move(new Vector3(0, 0, -1), new Vector3(-90, 0, 0));
     }
 
 }
