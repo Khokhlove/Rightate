18f7817f02d53249136758185bd6b1a9774c30dd
Dmitry Ablaev <36304189+Rubell1on@users.noreply.github.com>
2021-05-24 15:51:23 +0700

    Добавлен динамический задник с кубами

---
+++ Assets/CubesBackground.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CubesBackground: MonoBehaviour
+{
+    public GameObject template;
+    public List<GameObject> cubes;
+
+    void Start()
+    {
+        InitCubes();
+        transform.position = new Vector3(11, 8, 290);
+        transform.rotation = Quaternion.Euler(new Vector3(45, 200, 0));
+        transform.localScale = new Vector3(2, 2, 2);
+    }
+
+    void InitCubes()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Vector3 pos = new Vector3(i, 0, j);
+                GameObject temp = Instantiate(template, pos, Quaternion.identity, transform);
+                cubes.Add(temp);
+            }
+        }
+    }
+}
---
+++ Assets/FrequencyMover.cs
@@ -0,0 +1,43 @@
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CubesBackground))]
+[RequireComponent(typeof(AudioSource))]
+public class FrequencyMover : MonoBehaviour
+{
+    
+    public int leftBorder = 90;
+    [Range(1, 10)]
+    public float aplifier = 10f;
+
+    AudioSource audio;
+    CubesBackground cb;
+
+    private void Start()
+    {
+        cb = GetComponent<CubesBackground>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        float[] spectrum = new float[1024];
+        audio.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+
+        List<float> tempSpectrum = spectrum.ToList().GetRange(leftBorder, leftBorder + 100);
+        Move(tempSpectrum);
+    }
+
+    void Move(List<float> spectrum)
+    {
+        for (int i = 0; i < cb.cubes.Count; i++)
+        {
+            Transform t = cb.cubes[i].transform;
+            Vector3 oldPos = t.localPosition;
+            Vector3 newPos = new Vector3(oldPos.x, spectrum[i] * aplifier, oldPos.z);
+            t.localPosition = newPos;
+        }
+    }
+}
