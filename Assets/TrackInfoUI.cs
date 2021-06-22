d6171a6520f309db71a4a3c75c9cc46463b946df
Khokhlove <52717700+Khokhlove@users.noreply.github.com>
2021-06-22 17:14:39 +0700

    Рефакторинг файлов и скриптов

--- Assets/TrackInfoUI.cs
+++ Assets/TrackInfoUI.cs
@@ -7,16 +7,6 @@ public class TrackInfoUI : MonoBehaviour
 {
     public Text text;
 
-    void Start()
-    {
-        MusicContainer.GetInstance().trackChanged.AddListener(SetInfo);
-    }
-
-    void OnDestroy()
-    {
-        MusicContainer.GetInstance().trackChanged.RemoveListener(SetInfo);
-    }
-
     public void SetInfo(AudioTrack track)
     {
         text.text = $"{track.artistName} - {track.songName}";
