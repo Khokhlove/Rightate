c8426f5196d1d00db616e6430e0fccc450e7ae7e
Khokhlove <52717700+Khokhlove@users.noreply.github.com>
2021-06-22 18:18:02 +0700

    Крутой переход бэекграунда в игре!

---
+++ Assets/Scripts/MaterialAlphaController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MaterialAlphaController : MonoBehaviour
+{
+    public Material material;
+    [Range(0,10)] public float speed = 1;
+
+    float lerp;
+    Color color;
+
+    void Awake()
+    {
+        color = material.color;
+        material.color = new Color(color.r, color.g, color.b, 1);
+        color = material.color;
+    }
+    public void Hide()
+    {
+        StartCoroutine(_Hide());
+    }
+    public void Reveal()
+    {
+        StartCoroutine(_Reveal());
+    }
+
+    IEnumerator _Hide()
+    {
+        lerp = 0;
+        while (lerp < 1)
+        {
+            material.color = Color.Lerp(color, new Color(color.r, color.g, color.b, 0), lerp);
+            lerp += Time.deltaTime * speed;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    IEnumerator _Reveal()
+    {
+        lerp = 0;
+        while (lerp < 1)
+        {
+            material.color = Color.Lerp(color, new Color(color.r, color.g, color.b, 1), lerp);
+            lerp += Time.deltaTime * speed;
+            yield return new WaitForEndOfFrame();
+        }    
+    }
+
+}
