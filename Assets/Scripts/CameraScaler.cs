271b5a59d44fc4c45b63382896c7a9b915eb757c
Khokhlove <52717700+Khokhlove@users.noreply.github.com>
2021-06-22 21:14:02 +0700

    Новая музыка, скейлер

---
+++ Assets/Scripts/CameraScaler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraScaler : MonoBehaviour
+{
+    float lerp = 0;
+    public Camera camera;
+    float startSize;
+
+    public AnimationCurve pattern;
+    public float targetSize = 7.5f;
+    public float speed = 1;
+
+    void Start()
+    {
+        startSize = camera.orthographicSize;
+        camera.orthographicSize = targetSize;
+    }
+
+    public void Scale()
+    {
+        StartCoroutine(_Scale());
+    }
+
+    IEnumerator _Scale()
+    {
+        lerp = 0;
+        while (lerp < 1)
+        {
+            camera.orthographicSize = Mathf.Lerp(targetSize, startSize, lerp);
+            lerp += Time.deltaTime * speed;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+}
