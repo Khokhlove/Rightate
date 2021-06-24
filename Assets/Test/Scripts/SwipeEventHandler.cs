ba6ede1de3c74320d7a187288ea67e64e386bc10
Khokhlove <52717700+Khokhlove@users.noreply.github.com>
2021-06-24 18:21:25 +0700

    Рефакторинг свайпов

--- Assets/Test/Scripts/SwipeEventHandler.cs
+++ Assets/Test/Scripts/SwipeEventHandler.cs
@@ -3,30 +3,48 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Events;
 
-public class SwipeEventHandler : MonoBehaviour, IDragHandler, IBeginDragHandler
+public class SwipeEventHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
     public UnityEvent up;
     public UnityEvent down;
     public UnityEvent left;
     public UnityEvent right;
 
+    public Vector2 startDelta;
+    public Vector2 targetDelta;
+
     public void OnBeginDrag(PointerEventData eventData)
     {
-        if (Mathf.Abs(eventData.delta.x) > Mathf.Abs(eventData.delta.y))
-        {
-            if (eventData.delta.x > 0) right.Invoke();
-            else left.Invoke();
-        }
-        else
-        {
-            if (eventData.delta.y > 0) up.Invoke();
-            else down.Invoke();
-        }
+        startDelta = eventData.pressPosition;
+        //if (Mathf.Abs(eventData.delta.x) > Mathf.Abs(eventData.delta.y))
+        //{
+        //    if (eventData.delta.x > 0) right.Invoke();
+        //    else left.Invoke();
+        //}
+        //else
+        //{
+        //    if (eventData.delta.y > 0) up.Invoke();
+        //    else down.Invoke();
+        //}
     }
 
     public void OnDrag(PointerEventData eventData)
-
     {
 
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        targetDelta = eventData.position;
+        if (Mathf.Abs((startDelta.x - eventData.position.x)) > Mathf.Abs((startDelta.y - eventData.position.y)))
+        {
+            if ((startDelta.x - eventData.position.x) > 0) left.Invoke();
+            else right.Invoke();
+        }
+        else
+        {
+            if ((startDelta.y - eventData.position.y) > 0) down.Invoke();
+            else up.Invoke();
+        }
     }
 }
