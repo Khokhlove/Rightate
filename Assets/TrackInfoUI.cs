9325979c67756d59245e0ce87a66a97592d8dae7
rubell1on <dablaev@yandex.ru>
2021-06-24 12:39:02 +0700

    Рефакторинг

--- Assets/TrackInfoUI.cs
+++ Assets/TrackInfoUI.cs
@@ -7,6 +7,16 @@ public class TrackInfoUI : MonoBehaviour
 {
     public Text text;
 
+    private void Start()
+    {
+        MusicContainer.GetInstance().trackChanged.AddListener(SetInfo);
+    }
+
+    private void OnDestroy()
+    {
+        MusicContainer.GetInstance().trackChanged.RemoveListener(SetInfo);
+    }
+
     public void SetInfo(AudioTrack track)
     {
         text.text = $"{track.artistName} - {track.songName}";
